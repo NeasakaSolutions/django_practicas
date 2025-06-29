987e80e15aeaf38b66282e13119a628a14be5764
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-06-28 19:43:20 -0600

    Se agregaron urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,5 +5,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Ruta inicial no lleva nada entre las comillas
-    path('', include('home.urls')) # Manda a llamar al archivo urls de la app home
+    path('', include('home.urls')), # Manda a llamar al archivo urls de la app home
+    path('api/v1/', include('ejemplo.urls')) # localhost/api/v1/recetas
 ]
