e69d53678a7dc94f3573723fee3d7ba9daf902d5
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-06-28 21:16:05 -0600

    Se agrego la ruta para la nueva clase

--- backend/ejemplo/urls.py
+++ backend/ejemplo/urls.py
@@ -1,7 +1,12 @@
 # Importaciones:
 from django.urls import path
 from ejemplo.views import Class_Ejemplo
+from ejemplo.views import Class_Ejemplo_Parametros
 
 urlpatterns = [
-    path('ejemplo', Class_Ejemplo.as_view()) # as_view manda a llamar a todos los metodos de la clase
+    path('ejemplo', Class_Ejemplo.as_view()), # as_view manda a llamar a todos los metodos de la clase
+    # Se le pueden pasar argumentos con la identacion "diamante" <>, para pasar mas de uno se puede hacer asi:
+    # <int:id>/<string:slug>   Se pueden usar otros tipos de datos
+    # IMPORTANTE: NO DEJAR ESPACIOS EN LOS DIAMANTES
+    path('ejemplo/<int:id>', Class_Ejemplo_Parametros.as_view())
 ]
