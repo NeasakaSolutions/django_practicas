8d850e6781cec53797521f08912e35f57fb48ffe
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-06-28 21:45:13 -0600

    Se modificaron las clases en views.py

--- backend/ejemplo/views.py
+++ backend/ejemplo/views.py
@@ -3,6 +3,7 @@
 # que permite crear endpoints RESTful con métodos HTTP como GET, POST, PUT y DELETE
 from rest_framework.views import APIView
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Creacion de clases:
 class Class_Ejemplo(APIView):
@@ -10,7 +11,18 @@ class Class_Ejemplo(APIView):
 
     # Método GET: se utiliza para obtener o listar registros
     def get(self, request):
-        return HttpResponse("Metodo GET")
+        # Se obtiene los parametros del metodo request y se accede solo con los metodos GET y get
+        # Por buenas practicas se pone el segundo parametro None
+        '''
+        return HttpResponse(f"Metodo GET | " 
+                            f"id = {request.GET.get('id', None)} | "
+                            f"slug = {request.GET.get('slug', None)}")
+        '''
+
+        # Forma mas segura para evitar dar informacion a los hackers
+        return JsonResponse({"estado": "ok", "mensaje" : f"Metodo GET | " 
+                                f"id = {request.GET.get('id', None)} | "
+                                f"slug = {request.GET.get('slug', None)}"})
 
     # Método POST: se utiliza para crear nuevos registros en el servidor
     def post(self, request):
