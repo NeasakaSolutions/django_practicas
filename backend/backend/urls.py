0fe9d66b364db9bf5e3877a56921eda7a6894b5d
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-06-29 19:06:51 -0600

    Se agregaron nuevas rutas para los archivos subidos por parte del cliente

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('', include('home.urls')), # Manda a llamar al archivo urls de la app home
     path('api/v1/', include('ejemplo.urls')) # localhost/api/v1/recetas
 ]
+
+# Url para configuracion de los archivos que se van a mostrar
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
