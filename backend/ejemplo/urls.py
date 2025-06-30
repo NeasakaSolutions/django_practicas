82cd4fe7535ba2939a308ce892173eacacdf29ae
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-06-29 19:08:29 -0600

    Se agregaron nuevas urls para mostrar los archivos subidos por el cliente

--- backend/ejemplo/urls.py
+++ backend/ejemplo/urls.py
@@ -2,11 +2,13 @@
 from django.urls import path
 from ejemplo.views import Class_Ejemplo
 from ejemplo.views import Class_Ejemplo_Parametros
+from ejemplo.views import Class_EjemploUpload
 
 urlpatterns = [
     path('ejemplo', Class_Ejemplo.as_view()), # as_view manda a llamar a todos los metodos de la clase
     # Se le pueden pasar argumentos con la identacion "diamante" <>, para pasar mas de uno se puede hacer asi:
     # <int:id>/<string:slug>   Se pueden usar otros tipos de datos
     # IMPORTANTE: NO DEJAR ESPACIOS EN LOS DIAMANTES
-    path('ejemplo/<int:id>', Class_Ejemplo_Parametros.as_view())
+    path('ejemplo/<int:id>', Class_Ejemplo_Parametros.as_view()),
+    path('ejemplo-upload', Class_EjemploUpload.as_view())
 ]
