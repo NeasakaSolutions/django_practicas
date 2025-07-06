f990cf5d358df70e335a43eb4998718dfd642d2e
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-07-06 02:15:51 -0600

    Se agrego la nueva app y sus configuraciones

---
+++ backend/categorias/views.py
@@ -0,0 +1,10 @@
+# Importaciones
+from rest_framework.views import APIView
+from django.http.response import JsonResponse
+
+# Create your views here.
+class Clase1(APIView):
+    
+    def get(self, request):
+        pass
+
